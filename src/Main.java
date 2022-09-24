298bbe06e5643d850092c51e3ea2bd7030c9c72b
len9888 <44541602+len9888@users.noreply.github.com>
2022-09-24 23:14:06 +1000

    second commit - Account finished, ui finish, deposit withdraw getbalance finish.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Account account1 = new Account(5000.0,  1423);
+        int userOption = 0;
 
-        System.out.println(account1.getBalance());
-        account1.deposit(700.0);
-        System.out.println(account1.getBalance());
-        account1.withdraw(2000.0);
-        System.out.println(account1.getBalance());
+        Account account1 = new Account(5000.0, 1423);
+
+        ATM atm = new ATM();
+
+        Scanner scanner = new Scanner(System.in);
+
+        if (account1.validatePin(scanner.nextInt())) {
+            do {
+                atm.menu();
+                userOption = scanner.nextInt();
+                switch (userOption) {
+                    case 1:
+                        atm.withdrawMenu();
+                        double withdrawAmount = scanner.nextDouble();
+                        account1.withdraw(withdrawAmount);
+                        break;
+                    case 2:
+                        atm.depositMenu();
+                        double depositCreditAmount = scanner.nextDouble();
+                        account1.deposit(depositCreditAmount);
+                        break;
+                    case 3:
+                        atm.balanceMenu(account1.getBalance());
+                        break;
+                    case 4:
+                        atm.exitMenu();
+                        break;
+                    default:
+                        atm.invalidMessageMenu();
+                        break;
+                }
+            }
+            while (userOption != 4);
+        }
     }
 }
